c21e3d7040405969fc55aaf358ea337b997bb272
Frans AHW <franswinata6@gmail.com>
2022-07-11 09:50:23 +0700

    Update Contribute.jsx

--- components/Contribute.jsx
+++ components/Contribute.jsx
@@ -4,7 +4,7 @@ const Contribute = () =>
 {
   return (
     <div className="text-center fixed bottom-10 right-10 hidden lg:block">
-      <a href="https://github.com/fransachmadhw/Blogshot" target="_blank" className="transition duration-500 ease transform hover:-translate-y-2 flex items-center bg-white text-sm shadow-lg font-medium rounded-full text-black px-6 py-2 cursor-pointer">
+      <a href="https://github.com/fransachmadhw/blogshot-clone" target="_blank" className="transition duration-500 ease transform hover:-translate-y-2 flex items-center bg-white text-sm shadow-lg font-medium rounded-full text-black px-6 py-2 cursor-pointer">
         <span className="mr-1">Contribute on</span>
         <img src="/bxl-github.svg" alt="github-logo" width={20} className="github-logo" />
       </a>
